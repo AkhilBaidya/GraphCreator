1ea00982e758760bb373f175f02db512df9ec7a1
Akhil Baidya <akhilbaidya101@gmail.com>
2024-05-27 00:03:31 -0700

    a testing ground

---
+++ playground.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include "graph.h";
+
+using namespace std;
+
+int main() {
+
+  bool running = true;
+  char command[10];
+
+  while (running) {
+
+  }
+
+  
+  return 0;
+}
