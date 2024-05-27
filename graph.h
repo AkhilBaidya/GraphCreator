d26430252caa766ebe7692171278859b5f184859
Akhil Baidya <akhilbaidya101@gmail.com>
2024-05-26 22:15:31 -0700

    functions for the graph

--- graph.h
+++ graph.h
@@ -14,3 +14,22 @@ struct edge {
   int weight;
 };
 
+struct location {
+
+}
+
+class graph {
+
+ public:
+  graph(); //constructor
+  ~graph(); //destructor
+  void addVertex(char input); //add vertex
+  void remVertex(char input); //delete vertex
+  void addEdge(char first, char second); //add edge
+  void remEdge(char first, char second); //delete edge
+  void print(); //print out adjacency list
+  void shortestPath(char first, char last);
+  
+ private:
+  
+};
