8199aea63c4e0831d2e1f7ce8b734e1aa8478b6e
Akhil Baidya <akhilbaidya101@gmail.com>
2024-05-31 17:02:59 -0700

    Commented

--- playground.cpp
+++ playground.cpp
@@ -1,3 +1,12 @@
+/*Graph Creator Playground - Akhil Baidya
+
+Date of Submission: 5/31/24
+
+Notes: In this file, the user can create a graph and interact with it using several commands such as "mkvertex," "mkedge," "rmvertex," "rmedge," "print," "getshortestpath," and "quit."
+
+Manual: Refer to README file
+ */
+
 #include <iostream>
 #include <cstring>
 #include "graph.h"
@@ -6,71 +15,91 @@ using namespace std;
 
 int main() {
 
-  bool running = true;
-  char command[10];
-  graph* data = new graph();
+  //SET-UP:
+  bool running = true; //this loops the command-entry
+  char command[10]; //this will store user commands
+  graph* data = new graph(); //create an empty graph
 
+  cout << "Welcome to Graph Creator Ver. 1" << endl;
+  
+  //MAIN LOOP:
   while (running) {
-    cout << "What would you like to do? addvertex, addedge, remvertex, remedge, print, getpath" << endl;
-    cin >> command;
-    if (!strcmp(command, "addvertex")) {
-      cout << "enter vertex name" << endl;
-      char answer;
-      cin >> answer;
+    cout << "What would you like to do? (Options: mkvertex, mkedge, rmvertex, rmedge, print, getshortestpath, quit)" << endl;
+    cin >> command; //get user's command
 
-      data -> addVertex(answer);
+    //Adding a vertex:
+    if (!strcmp(command, "mkvertex")) {
+      cout << "Please enter vertex name (single character, unique from other vertices): " << endl;
+      char answer;
+      cin >> answer; //read in vertex name
+      
+      data -> addVertex(answer); //create vertex
     }
-    else if (!strcmp(command, "addedge")) {
+
+    //Adding an edge:
+    else if (!strcmp(command, "mkedge")) {
       char from;
       char to;
-      int weight;
-      cout << "from where?" << endl;
+      int weight; //data to collect
+
+      //Get data:
+      cout << "From where? (give vertex name)" << endl;
       cin >> from;
-      cout << "to where?" << endl;
+      cout << "To where? (give vertex name)" << endl;
       cin >> to;
-      cout << "of what weight?" << endl;
+      cout << "Of what weight? (enter an integer, make sure that max sum of edges in graph is under 9999)" << endl;
       cin >> weight;
 
-      data -> addEdge(from, to, weight);
-      
+      data -> addEdge(from, to, weight); //create edge    
     }
-    else if (!strcmp(command, "remvertex")) {
-      cout << "enter vertex name" << endl;
-      char answer;
-      cin >> answer;
 
-      data -> remVertex(answer);
+    //Removing a vertex:
+    else if (!strcmp(command, "rmvertex")) {
+      cout << "Please enter vertex name: " << endl;
+      char answer;
+      cin >> answer; //read in vertex name
+
+      data -> remVertex(answer); //remove vertex
     }
-    else if (!strcmp(command, "remedge")) {
+
+    //Removing an edge:
+    else if (!strcmp(command, "rmedge")) {
       char from;
       char to;
-      cout << "from where?" << endl;
+
+      //Get data:
+      cout << "From where? (give vertex name)" << endl;
       cin >> from;
-      cout << "to where?" << endl;
+      cout << "To where? (give vertex name)" << endl;
       cin >> to;
     
-      data -> remEdge(from, to);
+      data -> remEdge(from, to); //remove edge
       
     }
+
+    //Print graph as table:
     else if (!strcmp(command, "print")) {
       data -> print();
     }
 
+    //Quit program:
     else if (!strcmp(command, "quit")) {
-      running = false;
+      running = false; //end loop
       cout << "Quitting!" << endl;
     }
 
-    else if (!strcmp(command, "getpath")) {
+    //Print shortest path between two vertices:
+    else if (!strcmp(command, "getshortestpath")) {
       char from;
       char to;
-      cout << "from where?" << endl;
+
+      //Get data:
+      cout << "From where? (give vertex name)" << endl;
       cin >> from;
-      cout << "to where?" << endl;
+      cout << "To where? (give vertex name)" << endl;
       cin >> to;
     
-      data -> shortestPath(from, to);
-   
+      data -> shortestPath(from, to); //get shortest path
     }
   }
   return 0;
